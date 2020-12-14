79b88622ebcda93876c9a9d638ef4aaffab312cf
André Vieira <andreluizhillerv@gmail.com>
2020-12-14 20:25:44 -0300

    Mudanças iniciais incluindo: base HTML e CSS, inicio da lógica no javascript

---
+++ main.js
@@ -0,0 +1,25 @@
+const start = document.getElementById('start');
+
+const startDiscs = (n) => {
+    let newDisc = document.createElement('div');
+    
+    newDisc.classList.add('disk')
+    newDisc.classList.add('disk'+ n);
+    start.appendChild(newDisc);
+}
+
+const startGame = () => {
+    start.innerHTML = "";
+    
+    if (numberOfDisks.value > 8) {
+        alert('Escolha um valor entre 3 e 8!');
+        return
+    }
+
+    for (let i = numberOfDisks.value; i > 0; i--) {
+        startDiscs(i);
+    }
+}
+
+const startBtn = document.getElementById('startGameBtn');
+startBtn.addEventListener('click', startGame);
