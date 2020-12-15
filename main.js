8794e3854f5105247026199b67d7a09381252c54
Silvio Romano <silvioromanoneto@gmail.com>
2020-12-14 21:11:04 -0300

    lógica JS

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const start = document.getElementById('start');
+const offset = document.getElementById('offset');
+const end = document.getElementById('end');
 
 const startDiscs = (n) => {
     let newDisc = document.createElement('div');
@@ -21,5 +23,48 @@ const startGame = () => {
     }
 }
 
+// const firstTowerClick = (event) => {
+//     let firstTower = event.currentTarget;
+
+//     if (firstTower === start) {
+//         let title = document.querySelector('.bar-title div:first-child');
+//         title.classList.add('title-shadow');
+//     } else if (firstTower === offset) {
+//         let title = document.querySelector('.bar-title div:nth-child(2)');
+//         title.classList.add('title-shadow');
+//     } else {
+//         let title = document.querySelector('.bar-title div:last-child');
+//         title.classList.add('title-shadow');
+//     }
+// }
+
 const startBtn = document.getElementById('startGameBtn');
 startBtn.addEventListener('click', startGame);
+
+const removeTarget = document.querySelector('section#towers');
+removeTarget.addEventListener('click', function(e) {
+    let targetClicked;
+    if (e.target.classList.contains('disk')){
+        let parentTarget = e.target.parentNode;
+        targetClicked = parentTarget.lastElementChild;
+    } else {
+        targetClicked = e.target.lastElementChild;
+    }
+    
+    return targetClicked;
+})
+
+const pasteTarget = document.querySelector('section#towers');
+pasteTarget.addEventListener('click', function(e) {
+    let targetClicked;
+    if (e.target.classList.contains('disk')){
+        let parentTarget = e.target.parentNode;
+        targetClicked = parentTarget.lastElementChild;
+    } else {
+        targetClicked = e.target.lastElementChild;
+    }
+    
+    return targetClicked;
+});
+
+
